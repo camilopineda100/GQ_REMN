d685410c2eb4d19d74e7a9f6cce87024390d12cb
Sebastian Pineda <sebastian@fullstacklabs.co>
2021-04-13 09:45:31 -0500

    feat: add user from react

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const addUserHandler = () => {
+    const userData = {
+      email: "email2@email.com",
+      password: "testing123"
+    }
+
+    axios({
+      url: '/graphql',
+      method: 'POST',
+      data:{
+        query:`
+          mutation {
+            addUser(userInput:{
+              email:"${userData.email}"
+              password:"${userData.password}"
+            }){
+              _id
+              email
+              password
+            }
+          }
+        `
+      }
+    }).then( response => {
+      console.log(response.data)
+    }).catch( err => {
+      console.log(err)
+    })
+    console.log('add user')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={addUserHandler}
+      > 
+        Add User
+      </button>
     </div>
   );
 }
