0a34db5710c448d87e8aedbf427659ad37963a1a
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-13 07:58:37 -0500

    feat: setup playground and first resolvers and queries

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const bodyParser = require('body-parser')
+const { buildSchema } = require('graphql')
+const expressPlayground = require('graphql-playground-middleware-express').default
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+
 app.use('/graphql', 
     graphqlHTTP({
-        schema: '',
-        rootValue: 'resolvers',
+        schema: buildSchema(`
+            type RootQuery {
+                hello: String
+            }
+
+            type RootMutation {
+                someMutation: String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            hello: () => {
+                return 'Hello world'
+            }
+        },
         graphiql: true
     })
 )
