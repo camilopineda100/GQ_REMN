6c96f4c74686b9814dadf83b7de63948b3e0c6ef
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-13 10:21:36 -0500

    feat: express apollo server

--- server/server.js
+++ server/server.js
@@ -1,77 +1,71 @@
 const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const bodyParser = require('body-parser')
-const { buildSchema } = require('graphql')
-const expressPlayground = require('graphql-playground-middleware-express').default
+const { ApolloServer, gql } = require('apollo-server-express')
 const mongoose = require('mongoose')
 const User = require('./models/user')
 
+const typeDefs = gql`
+    type Query {
+        user(id: ID!): User!
+    }
+
+    type Mutation {
+        addUser(userInput: UserInput!): User!
+    }
+
+    type User {
+        _id: ID!
+        email: String!
+        password: String!
+    }
+
+    input UserInput {
+        email: String!
+        password: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        user: async (parent, args, context, info) => {
+            try {
+                const user = await User.findOne({ _id: args.id})
+                return {
+                    ...user._doc
+                }
+            } catch (err) {
+                throw err
+            }
+        },
+        
+    },
+    Mutation: {
+        addUser: async (parent, args, context, info) => {
+            try {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: args.userInput.password,
+                })
+
+                const result = await user.save()
+
+                return {
+                    ...result._doc
+                }
+            } catch(err) {
+                throw err
+            }
+        },
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
 
 const app = express()
 
-app.use(bodyParser.json())
-
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
-
-
-app.use('/graphql', 
-    graphqlHTTP({
-        schema: buildSchema(`
-            type RootQuery {
-                user(id: ID!): User!
-            }
-
-            type RootMutation {
-                addUser(userInput: UserInput!): User!
-            }
-
-            type User {
-                _id: ID!
-                email: String!
-                password: String!
-            }
-
-            input UserInput {
-                email: String!
-                password: String!
-            }
-
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
-        rootValue: {
-            user: async (args) => {
-                try {
-                    const user = await User.findOne({ _id: args.id})
-                    return {
-                        ...user._doc
-                    }
-                } catch (err) {
-                    throw err
-                }
-            },
-            addUser: async (args) => {
-                try {
-                    const user = new User({
-                        email: args.userInput.email,
-                        password: args.userInput.password,
-                    })
-
-                    const result = await user.save()
-
-                    return {
-                        ...result._doc
-                    }
-                } catch(err) {
-                    throw err
-                }
-            },
-        },
-        graphiql: true
-    })
-)
+server.applyMiddleware({ app })
 
 const PORT = process.env.PORT || 5000
 
@@ -86,4 +80,3 @@ mongoose.connect(`mongodb+srv://graphqluser:testing123@cluster0.mvizr.mongodb.ne
     console.log(error)
 
 })
-
