b49cc9920394e49c3ac01e88d8eae742fbb19fb4
Sebastian Pineda <camilopineda100@gmail.com>
2021-04-13 08:50:47 -0500

    feat: add user and get user connecting to mongoose

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const { buildSchema } = require('graphql')
 const expressPlayground = require('graphql-playground-middleware-express').default
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
-const PORT = process.env.PORT || 5000
 
 const app = express()
 
@@ -13,15 +13,27 @@ app.use(bodyParser.json())
 
 app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 
+
 app.use('/graphql', 
     graphqlHTTP({
         schema: buildSchema(`
             type RootQuery {
-                hello: String
+                user(id: ID!): User!
             }
 
             type RootMutation {
-                someMutation: String
+                addUser(userInput: UserInput!): User!
+            }
+
+            type User {
+                _id: ID!
+                email: String!
+                password: String!
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
             }
 
             schema {
@@ -30,13 +42,38 @@ app.use('/graphql',
             }
         `),
         rootValue: {
-            hello: () => {
-                return 'Hello world'
-            }
+            user: async (args) => {
+                try {
+                    const user = await User.findOne({ _id: args.id})
+                    return {
+                        ...user._doc
+                    }
+                } catch (err) {
+                    throw err
+                }
+            },
+            addUser: async (args) => {
+                try {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: args.userInput.password,
+                    })
+
+                    const result = await user.save()
+
+                    return {
+                        ...result._doc
+                    }
+                } catch(err) {
+                    throw err
+                }
+            },
         },
         graphiql: true
     })
 )
+
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(`mongodb+srv://graphqluser:testing123@cluster0.mvizr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
